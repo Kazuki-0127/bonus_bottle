149d696bbeda19eb3df83daf672bb4bf264e4fd7
太田 一樹 <kazuki.soccer1.27@gmail.com>
2020-02-28 14:53:03 +0900

    no message

---
+++ bonus_bottle.rb
@@ -0,0 +1,28 @@
+#buy=gets.chomp.to_i
+buy=ARGV[0].to_i
+
+amari=buy%3
+bonus=buy/3
+
+can=bonus
+
+
+while true
+    goukei=amari+bonus
+
+    bonus=goukei/3
+    amari=goukei%3
+
+    can=can+bonus
+
+    if goukei<3
+        break
+    end
+    
+end
+
+total=buy+can
+
+puts total
+
+ARGV[0]
